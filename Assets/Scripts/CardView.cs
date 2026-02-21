24fe37f1dc2226da5bd472a874be3b14a3ccb32b
JusticeABA <justice20041409@gmail.com>
2026-02-21 18:01:29 +0100

    d

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+   [SerializeField] private SpriteRenderer _spriteRenderer; 
+
+    [Serialize
+
+    void Update()
+    {
+
+    }
+}
